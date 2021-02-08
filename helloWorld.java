632eebc7e619a0ec331f6cd5e9586470e17a4b0d
Colby Jackson <cjack114@email.cpcc.edu>
2021-02-08 10:49:45 -0500

    Revert "Update helloWorld.java"
    This reverts commit ea71da8a787ee7f5dbd1eb35b72cbc8dd852f531.

--- helloWorld.java
+++ helloWorld.java
@@ -3,13 +3,5 @@ public class helloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello World");
-      
-      String name = "Colby Jackson";
-      
-      System.out.println(name);
-      
-      int age = 27;
-      
-      System.out.println(age);
    }
 }
